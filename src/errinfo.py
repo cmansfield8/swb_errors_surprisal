c74bf9259a66b56c18a8c96ef35f91496c202353
Courtney Mansfield <cmansfield8@gmail.com>
2020-04-13 16:32:52 -0700

    style changes and surprisal calculation
    -enums looks much cleaner now and print more informatively
    -surprisal calculations align with the early / corrected (MS) doc rather
    than error type

--- src/errinfo.py
+++ src/errinfo.py
@@ -12,43 +12,49 @@ from enum import Enum
 import util
 
 
-class ErrType(Enum):
+class Err(Enum):
 	MIX = -1
 	INS = 0
 	DEL = 1
 
 	
-class ErrShape(Enum):
+class Lex(Enum):
+	EOS = -2
 	MIX = -1
-	FUNCTION = 0
-	CONTENT = 1
-	DISCOURSE = 2
+	FUNC = 0
+	CONT = 1
+	DISC = 2
+	OTHER = 3
 	
 
 class Trans:
 
 	def __init__(self):
 		self.tokens = list()
-		self.error_shapes = list()
-		self.lstm_scores = list()
-		self.scores = list()
+		self.shapes = list()
+		self.nn_scores = list()
+		self.ngram_scores = list()
 
 	def summarize_shape(self):
-		if len(set(self.error_shapes)) == 1:
-			return self.error_shapes[0]
-		return ErrShape.MIX.value
+		if len(set(self.shapes[:-1])) == 1:
+			return self.shapes[0]
+		return Lex.MIX.name
 		
 	def set_token(self, token, shape):
 		self.tokens.append(token)
-		self.error_shapes.append(shape)
+		self.shapes.append(Lex(int(shape)).name)
 		
-	def set_score(self, ngram_value, lstm_value):
-		self.scores.append(ngram_value)
-		self.lstm_scores.append(lstm_value)
+	def set_score(self, ngram_value, nn_value):
+		self.ngram_scores.append(ngram_value)
+		self.nn_scores.append(nn_value)
 		
-	def get_header(self, dataset):
+	def get_header(self, dtype):
+		if dtype == 'ptb':
+			prefix = ''
+		else:
+			prefix = 'ms_'
 		keys = list(self.__dict__.keys())
-		return [dataset + "_" + x for x in keys]
+		return [prefix + x for x in keys]
 		
 	def get_values(self):
 		return list(self.__dict__.values())
@@ -62,46 +68,44 @@ class ErrSeq:
 		self.avg_type = None
 		self.avg_shape = None
 		self.sup = None
-		self.lstm_sup = None
+		self.nn_sup = None
 		self.error_type = list()
 		self.ptb = Trans()
 		self.ms = Trans()
 		
 	def _summarize_type(self):
 		if "INS" in self.error_type and "DEL" in self.error_type:
-			self.avg_type = ErrType.MIX.value
+			self.avg_type = Err.MIX.name
 		elif "DEL" in self.error_type:
-			self.avg_type = ErrType.DEL.value
+			self.avg_type = Err.DEL.name
 		else:
-			self.avg_type = ErrType.INS.value
+			self.avg_type = Err.INS.name
 			
 	def _summarize_shape(self):
-		if self.avg_type == ErrType.MIX.value:
-			self.avg_shape = ErrShape.MIX.value
-		elif self.avg_type == ErrType.INS.value:
+		if self.avg_type == Err.MIX.name:
+			self.avg_shape = Lex.MIX.name
+		elif self.avg_type == Err.INS.name:
 			self.avg_shape = self.ptb.summarize_shape()
 		else:
 			self.avg_shape = self.ms.summarize_shape()
 			
 	def _get_surprisal_value(self):
-		# scores are inversed based on if it's an INS/SUB or DEL/SUB
-		if self.avg_type == ErrType.DEL.value:
-			s1, s2 = self.ms, self.ptb
-		else:
-			s1, s2 = self.ptb, self.ms
-		self.sup = util.get_sup_diff(s1.scores, s2.scores)
-		self.lstm_sup = util.get_sup_diff(s1.lstm_scores, s2.lstm_scores)
+		s1, s2 = self.ptb, self.ms
+		self.sup = util.get_sup_diff(s1.ngram_scores, s2.ngram_scores)
+		self.nn_sup = util.get_sup_diff(s1.nn_scores, s2.nn_scores)
 		
-	def summarize(self):
+	def make_summary(self):
 		self._summarize_type()
 		self._summarize_shape()
 		self._get_surprisal_value()
 	
 	def add_type(self, value):
 		self.error_type.append(value)
-		
-	def get_header(self):  # does not include ptb and ms objects, since these would not be printed
+
+	# does not include ptb and ms Trans objects
+	def get_header(self):
 		return list(self.__dict__.keys())[:-2]
 
+	# does not include ptb and ms Trans objects
 	def get_values(self):
 		return list(self.__dict__.values())[:-2]
